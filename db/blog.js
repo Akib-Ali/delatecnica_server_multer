6956f63f4f7fea59947418891c0fdb6c4e52c192
Akib <akib7599@gmail.com>
2023-05-19 14:26:37 +0530

    add delatecnica server multer

---
+++ db/blog.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose')
+const blogSchema = new mongoose.Schema({
+
+    blog_title: {
+        type: String,
+        required:true
+    },
+
+    blog_slug: {
+        type: String,
+        required:true,
+    },
+    blog_summary: {
+        type: String,
+        required:true
+    },
+    blog_keyword: {
+        type: String,
+        required:true
+    },
+     image:String,
+
+    blog_content: {
+        type: String,
+        required:true
+    },
+    createdAt:{
+        type:Date,
+        default:Date.now()
+    }
+
+})
+
+module.exports = mongoose.model("dellatecnica_blogs",blogSchema)
---
+++ db/conn.js
@@ -0,0 +1,4 @@
+const mongoose = require("mongoose")
+mongoose.connect("mongodb+srv://akib7599:YafieDNY2KyN9aHU@cluster0.hglpxum.mongodb.net/")
+
+// mongoose.connect("mongodb://127.0.0.1:27017/dellatecnica_db")
